c4252762523e8c082843b175025adc17f732b54e
rico <r03292760@gmail.com>
2026-08-17 14:32:23 +0200

    feat: add fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Rico Scripts'
+description 'Standalone dyno resource with server validated measurements'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_script 'client/main.lua'
+server_script 'server/main.lua'
+

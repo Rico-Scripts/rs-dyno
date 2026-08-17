bfb8d3ed068a3f8b7a42bbc15e836517ec70b34b
rico <r03292760@gmail.com>
2026-08-17 14:32:28 +0200

    feat: add config.lua

---
+++ config.lua
@@ -0,0 +1,9 @@
+Config = {}
+Config.Command = 'dyno'
+Config.TestSeconds = 10
+Config.CooldownSeconds = 30
+Config.MaxSpeedKmh = 450.0
+Config.Webhook = ''
+Config.RequireAce = false
+Config.Ace = 'rs.dyno'
+

701f1fc4202fbb68683d6ce18827fe97590b7727
rico <r03292760@gmail.com>
2026-08-22 08:13:17 +0200

    feat: route webhooks through central logger

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,11 +2,11 @@ fx_version 'cerulean'
 game 'gta5'
 author 'Rico Scripts'
 description 'Standalone motorcycle dyno for Moto Workshop'
-version '2.0.0'
+version '2.0.1'
 lua54 'yes'
 ui_page 'nui/index.html'
 shared_scripts { '@ox_lib/init.lua', 'config.lua' }
 client_scripts { 'client/main.lua' }
-server_scripts { '@oxmysql/lib/MySQL.lua', 'server/main.lua' }
+server_scripts { '@oxmysql/lib/MySQL.lua', '@rs_discordlogs/server/intercept.lua', 'server/main.lua' }
 files { 'nui/index.html', 'nui/style.css', 'nui/app.js' }
-dependencies { 'rs-bikemechanic', 'ox_lib', 'ox_target', 'oxmysql' }
+dependencies { 'rs-bikemechanic', 'ox_lib', 'ox_target', 'oxmysql', 'rs_discordlogs' }
